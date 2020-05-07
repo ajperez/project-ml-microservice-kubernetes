c0b57e82ceb450f13fb44b60ce1bbdcfba75025d
ajperez <ajperezmontilla@gmail.com>
2020-05-07 21:56:54 +0200

    Update docker image
    Run and update docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=ajpm1977/ms-ajpm:latest
+
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag ms-ajpm:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
