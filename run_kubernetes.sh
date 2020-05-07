8c54fafcddf0ba47d9121a5442c2072281d3ae47
ajperez <ajperezmontilla@gmail.com>
2020-05-07 22:15:20 +0200

    Update kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,21 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=ajpm1977/ms-ajpm:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run ms-ajpm\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=ms-ajpm
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward ms-ajpm 8000:80
 
