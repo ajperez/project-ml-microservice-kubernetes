8621ef6ee08943ac06707377350a60e53b13a2df
ajperez <ajperezmontilla@gmail.com>
2020-05-09 22:32:47 +0200

    Updated files
    Updated rund_kubernetes.sh
    Updated output files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,10 +9,7 @@ dockerpath=ajpm1977/ms-ajpm:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run ms-ajpm\
-    --generator=run-pod/v1\
-    --image=$dockerpath\
-    --port=80 --labels app=ms-ajpm
+kubectl create deployment ms-ajpm --image=$dockerpath
 
 
 # Step 3:
@@ -21,5 +18,5 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward ms-ajpm 8000:80
+kubectl port-forward deployment/ms-ajpm 8000:80
 
